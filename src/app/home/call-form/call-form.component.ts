f334956f74da1b8bcf1d1d93f719c4faa0d6b574
Julian Mendoza <jdmendoza_90@hotmail.com>
2025-07-28 16:53:33 -0500

    Better styled call form

--- src/app/home/call-form/call-form.component.ts
+++ src/app/home/call-form/call-form.component.ts
@@ -13,16 +13,18 @@ export class CallFormComponent {
       Validators.required,
       Validators.minLength(3),
     ]),
-    callId: new FormControl('', [Validators.required, Validators.minLength(5)]),
+    callId: new FormControl('', [Validators.minLength(5)]),
   });
-
-  joinCall() {
-    // Logic to join a call
-    console.log('Joining call...');
-  }
 
   createCall() {
     // Logic to create a call
     console.log('Creating call...');
+    console.log('callform', this.callForm.valid);
+  }
+
+  joinCall() {
+    // Logic to join a call
+    console.log('Joining call...');
+    console.log('callform', this.callForm);
   }
 }
